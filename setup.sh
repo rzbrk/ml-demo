931faec2dd898503a877fbd184087cb662dedef2
Jan Grosser <email@jan-grosser.de>
2021-10-21 20:59:37 +0200

    First commit

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install libhdf5-dev libc-ares-dev libeigen3-dev gcc gfortran libgfortran5 \
+	libatlas3-base libatlas-base-dev libopenblas-dev libopenblas-base libblas-dev \
+        liblapack-dev cython3 libatlas-base-dev openmpi-bin libopenmpi-dev python3-
